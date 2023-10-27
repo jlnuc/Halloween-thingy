b68d47b0903a46cee133adedea625331b5c298c7
Jack Lin <jack.lin11@myhunter.cuny.edu>
2023-10-27 11:10:36 -0400

    done

---
+++ Halloween project.py
@@ -0,0 +1,98 @@
+import sys
+import pygame
+import ctypes
+from pygame.locals import *
+pygame.init()
+fps = 300
+fpsClock = pygame.time.Clock()
+width, height = 1280, 920
+screen = pygame.display.set_mode((width, height), pygame.RESIZABLE)
+font = pygame.font.SysFont('Arial', 20)
+objects = []
+drawColor = [0, 0, 0]
+brushSize = 5
+brushSizeSteps = 3
+canvasSize = [800, 800]
+class Button():
+    def __init__(self, x, y, width, height, buttonText='Button', onclickFunction=None, onePress=False):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.onclickFunction = onclickFunction
+        self.onePress = onePress
+        self.fillColors = {
+            'normal': '#ffffff',
+            'hover': '#666666',
+            'pressed': '#333333',
+        }
+        self.buttonSurface = pygame.Surface((self.width, self.height))
+        self.buttonRect = pygame.Rect(self.x, self.y, self.width, self.height)
+        self.buttonSurf = font.render(buttonText, True, (20, 20, 20))
+        self.alreadyPressed = False
+        objects.append(self)
+
+    def process(self):
+        mousePos = pygame.mouse.get_pos()
+
+        self.buttonSurface.fill(self.fillColors['normal'])
+        if self.buttonRect.collidepoint(mousePos):
+            self.buttonSurface.fill(self.fillColors['hover'])
+
+            if pygame.mouse.get_pressed(num_buttons=3)[0]:
+                self.buttonSurface.fill(self.fillColors['pressed'])
+
+                if self.onePress:
+                    self.onclickFunction()
+                elif not self.alreadyPressed:
+                    self.onclickFunction()
+                    self.alreadyPressed = True
+            else:
+                self.alreadyPressed = False
+        self.buttonSurface.blit(self.buttonSurf, [
+            self.buttonRect.width/2 - self.buttonSurf.get_rect().width/2,
+            self.buttonRect.height/2 - self.buttonSurf.get_rect().height/2
+        ])
+        screen.blit(self.buttonSurface, self.buttonRect)
+def changebrushSize(dir):
+    global brushSize
+    if dir == 'greater':
+        brushSize += brushSizeSteps
+    else:
+        brushSize -= brushSizeSteps
+def save():
+    pygame.image.save(canvas, "canvas.png")
+buttonWidth = 120
+buttonHeight = 35
+buttons = [ ['Brush Larger', lambda: changebrushSize('greater')],
+    ['Brush Smaller', lambda: changebrushSize('smaller')],
+    ['Save', save],
+]
+for index, buttonName in enumerate(buttons):
+    Button(index * (buttonWidth + 10) + 10, 10, buttonWidth,
+           buttonHeight, buttonName[0], buttonName[1])
+canvas = pygame.image.load('pumpkin.jpg')
+running = True
+while running:
+    screen.fill((30, 30, 30))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    for object in objects:
+        object.process()
+    x, y = screen.get_size()
+    screen.blit(canvas, [x/2 - canvasSize[0]/2, y/2 - canvasSize[1]/2])
+    if pygame.mouse.get_pressed()[0]:
+        mx, my = pygame.mouse.get_pos()
+        dx = mx - x/2 + canvasSize[0]/2
+        dy = my - y/2 + canvasSize[1]/2
+        pygame.draw.circle(
+            canvas,
+            drawColor,
+            [dx, dy],
+            brushSize,
+        )
+  
+    pygame.display.flip()
+    fpsClock.tick(fps)
